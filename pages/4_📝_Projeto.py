d7f4e8cf349a45e5ce249d91ceaa1b5754587452
Didalice <130405706+Didalice@users.noreply.github.com>
2023-06-26 19:47:15 +0200

    Update 4_📝_Projeto.py

--- pages/4_📝_Projeto.py
+++ pages/4_📝_Projeto.py
@@ -18,35 +18,42 @@ st.sidebar.info(markdown)
 logo = "images/UNESCO.gif"
 st.sidebar.image(logo)
 def main():
-    st.title("Autor")
-    st.markdown("Ce programme a été développé par [votre nom] dans le cadre du [Projet Pluridisciplinaire].")
-    
-    st.subheader("Título do Projeto")
-    st.markdown("Le titre du projet est [insérer le titre du projet].")
-    
+    st.header("Projet Pluridisciplianire 2022.")
     st.subheader("Objetivo e Usuário")
-    st.markdown("L'objectif principal de ce projet est [insérer l'objectif principal du projet]. Les utilisateurs cibles de ce programme sont [insérer les utilisateurs cibles du programme].")
-    
+    st.write("L'objectif de ce projet est d'étudier une portion du Val de Loire située au niveau de l'Île de Béhuard. L'utilisateur de ce programme est intéressé par l'analyse graphique et les résultats obtenus à partir des travaux réalisés.")
+    st.write("Ce projet réalisé sur streamlit permet une visualisation plus interactive du rapport.")
     st.subheader("Tipo (aplicação web, plugin QGIS, caderno Jupyter/colab)")
-    st.markdown("Ce programme est une [insérer le type de programme] développé avec [insérer les technologies/utilitaires utilisés pour développer le programme].")
-    
+    st.write("Ce programme est une application web développée avec Streamlit.")
     st.subheader("Bibliotecas principais")
-    st.markdown("Les bibliothèques principales utilisées dans ce programme sont [insérer les bibliothèques principales utilisées dans le programme].")
-    
+    st.write("Les bibliothèques principales utilisées dans ce programme sont :")
+    st.write("- Streamlit")
+    st.write("- Leafmap")
+    st.write("- Pandas")
     st.subheader("Arquitetura")
-    st.markdown("L'architecture de ce programme est [insérer l'architecture du programme].")
-    
+    st.write("L'architecture de ce programme suit le modèle MVC (Modèle-Vue-Contrôleur), où Streamlit agit comme le contrôleur, Leafmap comme la vue pour la cartographie et Pandas pour la manipulation des données.")
     st.subheader("Dados de Entrada")
-    st.markdown("Les données d'entrée utilisées dans ce programme sont [insérer les données d'entrée utilisées dans le programme].")
-    
+    st.write("- Les données pour genere les cartes proviennent de l'analyse de deux photographies aérienne de l'IGN, nomenclature de Croine Land Cover")
+    st.write("- Le texte provient du [rapport du projet pluridisciplinaire](https://drive.google.com/file/d/1AAZHmcbd7jpFdqhTs5pqXbZqaX4RELUS/view?usp=sharing) effectué en France en 2022")
+    st.write("- Pour les cartes, j'ai utilisé Qgis pour pouvoir les exporter sous le formats Geojson")
     st.subheader("Funcionalidades")
-    st.markdown("Les fonctionnalités de ce programme comprennent [insérer les fonctionnalités du programme].")
-    
+    st.write("Les principales fonctionnalités de ce programme sont :")
+    st.write("- Affichage des photographies aériennes")
+    st.write("- Identification des unités paysagères")
+    st.write("- Caractérisation des paysages actuels et passés")
+    st.write("- Génération de cartes et de visualisations graphiques")
     st.subheader("Dados de Saída (atenção para os mapas)")
-    st.markdown("Les données de sortie générées par ce programme sont [insérer les données de sortie générées par le programme]. Il faut faire attention particulièrement aux [insérer les détails spécifiques sur les cartes ou les données de sortie liées aux cartes].")
+    st.write("Les données de sortie de ce programme comprennent :")
+    st.write("- Les unités paysagères identifiées")
+    st.write("- Les caractéristiques des paysages actuels et passés")
+    st.write("- Les cartes et visualisations graphiques générées")
+    
     
     st.subheader("Esboço da Interface")
-    st.markdown("L'esquisse de l'interface utilisateur de ce programme est [insérer l'esquisse de l'interface utilisateur].")
+    st.write("L'interface de ce programme comprend différentes sections, notamment :")
+    st.write("- Une barre latérale avec des informations sur le projet")
+    st.write("- Une section d'introduction avec des explications sur la zone étudiée")
+    st.write("- Des boutons pour afficher les photographies et les visualisations")
+    st.write("- Des marqueurs sur une carte pour identifier les unités paysagères")
 
 if __name__ == "__main__":
     main()
