f10eb2966c9b25edce976f35d8b288d5c3e4a241
Didalice <130405706+Didalice@users.noreply.github.com>
2023-06-26 08:49:29 +0200

    Update 1_🏞_Unités_paysagères.py

--- pages/1_🏞_Unités_paysagères.py
+++ pages/1_🏞_Unités_paysagères.py
@@ -77,7 +77,7 @@ with colu1:
     bouton()
     folium_static(m, width=470, height=400)
   if col1.button("Carte unité paysagère 2008"):
-    land_use_map='carte/pays_2008.geojson'
+    land_use_map='carte/pays_1982.geojson'
     m=folium.Map(location=[47.389468, -0.633296], zoom_start=14)
     tooltip = folium.GeoJsonTooltip(fields=['Unité'], aliases=['Land Use Class'])
     folium.GeoJson(land_use_map,name='land use map',style_function= lambda feature: {'fillColor':colors(feature),'fillOpacity':0.7, 'weight':0},tooltip=tooltip).add_to(m)
