3ab325aba406ca73e186f7f8fff122df5d8b98f1
Didalice <130405706+Didalice@users.noreply.github.com>
2023-06-26 08:48:14 +0200

    Update 2_🌾_Occupation_des_sols.py

--- pages/2_🌾_Occupation_des_sols.py
+++ pages/2_🌾_Occupation_des_sols.py
@@ -61,14 +61,14 @@ with colu2:
 
 
 with colu1:
-  if col1.button("Carte unité paysagère 1982"):
+  if col1.button("Carte occupation du sol 1982"):
     land_use_map='carte/occ_1982.geojson'
     m=folium.Map(location=[47.389468, -0.633296], zoom_start=14)
     tooltip = folium.GeoJsonTooltip(fields=['clc_niv3'], aliases=['Land Use Class'])
     folium.GeoJson(land_use_map,name='land use map',style_function= lambda feature: {'fillColor':colors(feature),'fillOpacity':0.7, 'weight':0},tooltip=tooltip).add_to(m)
     bouton()
     folium_static(m, width=470, height=400)
-  if col1.button("Carte unité paysagère 2008"):
+  if col1.button("Carte occupation du sol 2008"):
     land_use_map='carte/occ_2008.geojson'
     m=folium.Map(location=[47.389468, -0.633296], zoom_start=14)
     tooltip = folium.GeoJsonTooltip(fields=['clc_niv3'], aliases=['Land Use Class'])
