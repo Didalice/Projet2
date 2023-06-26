f45fdf52e2ce4c595515dab679f35c4ff32a7137
Didalice <130405706+Didalice@users.noreply.github.com>
2023-06-26 04:54:21 +0200

    Update 1_🌍_UNITÉS_PAYSAGÈRES.py

--- pages/1_🌍_UNITÉS_PAYSAGÈRES.py
+++ pages/1_🌍_UNITÉS_PAYSAGÈRES.py
@@ -83,5 +83,5 @@ with colu1:
                   tooltip=tooltips,
                   icon = folium.Icon(icon='star', color = 'black')
                  ).add_to(m)
-    folium_static(m, width=600, height=400)
+    folium_static(m, width=400, height=400)
 
