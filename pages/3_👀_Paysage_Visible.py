ae8ba35bbd39191e3dadfe0821388dcf195db20f
Didalice <130405706+Didalice@users.noreply.github.com>
2023-06-26 07:52:39 +0200

    Update 3_👀_Paysage_Visible.py

--- pages/3_👀_Paysage_Visible.py
+++ pages/3_👀_Paysage_Visible.py
@@ -49,7 +49,7 @@ if col1.button("Carte unité paysagère 2008"):
   land_use_map='carte/occ_2008.geojson'
   m=folium.Map(location=[47.389468, -0.633296], zoom_start=14)
   tooltip = folium.GeoJsonTooltip(fields=['clc_niv3'], aliases=['Land Use Class'])
-  folium.GeoJson(land_use_map,name='land use map',style_function= lambda feature: {'fillColor':'#e6004dff'),'fillOpacity':0.7, 'weight':0},tooltip=tooltip).add_to(m)
+  folium.GeoJson(land_use_map,name='land use map',style_function= lambda feature: {'fillColor':'#e6004dff','fillOpacity':0.7, 'weight':0},tooltip=tooltip).add_to(m)
   bouton()
   folium_static(m, width=470, height=400)
 
