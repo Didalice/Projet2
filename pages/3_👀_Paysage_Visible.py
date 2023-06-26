a708ac90ab850a5973b483d33ea8b8b88d4b32ab
Didalice <130405706+Didalice@users.noreply.github.com>
2023-06-26 08:41:47 +0200

    Update 3_👀_Paysage_Visible.py

--- pages/3_👀_Paysage_Visible.py
+++ pages/3_👀_Paysage_Visible.py
@@ -42,16 +42,16 @@ def opa(feature):
     return 1
   return 0
   
-if col1.button("Carte unité paysagère 1982"):
-  land_use_map='carte/occ_1982.geojson'
+if col1.button("Carte paysages visibles 1982"):
+  land_use_map='carte/pays_visibles_1982.geojson'
   m=folium.Map(location=[47.389468, -0.633296], zoom_start=14)
-  folium.GeoJson(land_use_map,name='land use map',style_function= lambda feature: {'fillColor':'#e6004dff','fillOpacity':0.5, 'weight':0},tooltip=tooltip).add_to(m)
+  folium.GeoJson(land_use_map,name='land use map',style_function= lambda feature: {'fillColor':'#e6004dff','fillOpacity':opa(feature), 'weight':0},tooltip=tooltip).add_to(m)
   bouton()
   folium_static(m)
 
 
-if col1.button("Carte unité paysagère 2008"):
-  land_use_map='carte/occ_2008.geojson'
+if col1.button("Carte paysages visibles 2008"):
+  land_use_map='carte/pays_visibles_1982.geojson'
   m=folium.Map(location=[47.389468, -0.633296], zoom_start=14)
   folium.GeoJson(land_use_map,name='land use map',style_function= lambda feature: {'fillColor':'#e6004dff','fillOpacity':0.5, 'weight':0},tooltip=tooltip).add_to(m)
   bouton()
