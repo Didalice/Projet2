0cfadb30a63286c7c17d9ba1c2af5591e0f12612
Didalice <130405706+Didalice@users.noreply.github.com>
2023-07-06 03:32:33 +0200

    Update 1_🏞_Unités_paysagères.py

--- pages/1_🏞_Unités_paysagères.py
+++ pages/1_🏞_Unités_paysagères.py
@@ -34,7 +34,7 @@ opp_dict = {'1': 0, '5': 0, '12': 0,'11': 0, '3': 0, '4': 0,'2': 0, '9': 0, '6':
 for i in labels:
     label = i.split(" - ")
     variable_name = 'show_' + str(label[0])
-    eval('checkbox_'+str(label[0])) = st.sidebar.checkbox(label[1])
+    eval('checkbox_'+str(label[0]) = st.sidebar.checkbox(label[1]))
     
     if eval('checkbox_'+str(label[0])) :
         opp_dict[str(label[0])] = 0.7
