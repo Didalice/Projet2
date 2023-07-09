8ea020cda749136353556b7981fd721f4af8a55b
Didalice <130405706+Didalice@users.noreply.github.com>
2023-07-09 03:29:45 +0200

    Update 2_🌾_Occupation_des_sols.py

--- pages/2_🌾_Occupation_des_sols.py
+++ pages/2_🌾_Occupation_des_sols.py
@@ -120,7 +120,6 @@ with colu2:
                     <span style="font-size: 10px;">{label}</span>
                 </div>''', unsafe_allow_html=True)
 with colu1:
-  annee = st.selectbox(sel_an, ["1982", "2008"])
   for i, label in enumerate(labels):
     column = co1 if i < len(labels)//4 else (co2 if i < 2*(len(labels)//4) else (co3 if i < 3*(len(labels)//4) else co4))
     show_label = column.checkbox(label.split(' - ')[1])
