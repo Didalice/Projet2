c99f71c9305baea217c4afbf56f9bd251b315b13
Didalice <130405706+Didalice@users.noreply.github.com>
2023-07-10 04:20:07 +0200

    Update 3_👀_Paysage_Visible.py

--- pages/3_👀_Paysage_Visible.py
+++ pages/3_👀_Paysage_Visible.py
@@ -105,6 +105,8 @@ elif annee == "2008":
 m=folium.Map(location=[47.389468, -0.633296], zoom_start=14)
 folium.GeoJson(land_use_map,name='land use map',style_function= lambda feature: {'fillColor':'#e6004dff','fillOpacity':1, 'weight':0}).add_to(m)
 bouton()
+folium.Circle(radius=5000,location=[-0.626613,47.391153],color='crimson').add_to(m)
+m
 folium_static(m)
 st.subheader(legende)
 st.markdown(f'''<div style="display: flex; align-items: center;">
